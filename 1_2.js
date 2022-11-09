35c3c8ed60b6f10b37aec89d605f8bd38e0591f4
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-11-09 20:40:05 -0300

    created '1_2' e linked with 'index'

---
+++ 1_2.js
@@ -0,0 +1,15 @@
+// 
+// *1.2* 
+// 
+
+// 1 - Se caminho 5Km e corro 2Km, passo o dia bem.
+
+console.log("1 - caminho_5Km_&&_corro_2Km, passo_o_dia_bem")
+
+// 2 - Se levanto cedo OU antes das 8, saio tranquilo.
+
+console.log("2 - levanto_cedo||antes_das_8, saio_tranquilo")
+
+// 3 - Se NÃO termino o trabalho ou NÃO tenho compromisso, saio tarde.
+
+console.log("3 - !termino_o_trabalho||!tenho_compromisso, saio_tarde")
